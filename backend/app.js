66c6b07a52f937d25e29adbd9bfd86c279888bca
Tharindudasundenawaka <tharindudasun1997@gmail.com>
2024-04-05 12:23:52 +0530

    Adding backend and frontend code

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+// const path = require("path")
+const express = require("express");
+const app = express();
+const port = 5000
+const bodyParser = require('body-parser');
+
+//set bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+    res.send("hello SlNightLife ");
+});
+
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+})
+
+
+
+
+app.use(cors()) 
